2159cd491fc745d0549afa955c62c20517d8f151
Andrew Lamb <andrew@nerdnetworks.org>
2022-01-21 13:49:16 -0500

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, sync::Arc, path::{Path}};
+
+use arrow::util::pretty::pretty_format_batches;
+use parquet::{file::serialized_reader::SerializedFileReader, arrow::{ParquetFileArrowReader, ArrowReader}};
+
+use clap::Parser;
+
+
+/// Dumps contents of parquet files to stdout using Rust implemetnation of arrow array reader
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the file to dump
+    #[clap(short, long)]
+    filename: String,
+}
+
+
+fn main()  {
+    let args = Args::parse();
+    if let Err(e) = test_parquet(&args.filename) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    } else {
+        std::process::exit(0);
+    }
+}
+
+/// Opens a parquet file, prints it to standardout
+fn test_parquet(filename: impl AsRef<Path>) -> parquet::errors::Result<()> {
+    let filename = filename.as_ref();
+    println!("Testing {:?}", filename);
+
+    let file = File::open(filename).unwrap();
+    let file_reader = SerializedFileReader::new(file)?;
+    let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(file_reader));
+
+    // TODO avoid buffering the entire thing
+    let record_batch_reader = arrow_reader.get_record_reader(2048)?;
+
+    let mut batches = vec![];
+    for maybe_record_batch in record_batch_reader {
+        batches.push(maybe_record_batch?);
+    }
+
+    println!("{}", pretty_format_batches(&batches)?);
+    Ok(())
 }
